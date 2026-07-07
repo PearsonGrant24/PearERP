c91c536f2b0d1778183f80ccb0f331249f7ff3d1
Peaerson Grant <pearsongrant23@gmail.com>
2026-07-07 20:56:24 +0200

    all

--- Frontend/src/common/layouts/MainLayout/Footer/Footer.tsx
+++ Frontend/src/common/layouts/MainLayout/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 
 
-export default function Sidebar() {
+export default function Footer() {
     return(
         <div>
             
--- Frontend/src/common/layouts/MainLayout/MainLayout.tsx
+++ Frontend/src/common/layouts/MainLayout/MainLayout.tsx
@@ -1,14 +1,17 @@
 import { Outlet } from "react-router-dom";
 import "./MainLayout.css";
 
+import Sidebar from "../MainLayout/Sidebar/Sidebar.tsx"
+import Navbar from "../MainLayout/Navbar/Navbar.tsx";
+import Breadcrumb from "./Breadcrumb/Breadcrumb.tsx";
+import Footer from "../MainLayout/Footer/Footer.tsx"
+
 export default function MainLayout() {
     return (
         <div className="layout">
 
             {/* Sidebar */}
-            <aside className="sidebar">
-                Sidebar
-            </aside>
+            <Sidebar/>
 
             {/* Main Section */}
             <div className="main">
