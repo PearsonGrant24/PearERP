fdb8909752cf016e77c15b2cdb7e456247033dbf
Peaerson Grant <pearsongrant23@gmail.com>
2026-07-18 22:20:09 +0200

    all

--- Frontend/src/common/layouts/MainLayout/Navbar/Navbar.tsx
+++ Frontend/src/common/layouts/MainLayout/Navbar/Navbar.tsx
@@ -1,21 +1,33 @@
 import "./Navbar.css";
 
+import SidebarToggle from "./components/SidebarToggle";
+
 export default function Navbar() {
+
     return (
+
         <header className="navbar">
 
             <div className="navbar__left">
-                Left
+
+                <SidebarToggle/>
+
             </div>
 
             <div className="navbar__center">
+
                 Center
+
             </div>
 
             <div className="navbar__right">
+
                 Right
+
             </div>
 
         </header>
+
     );
+
 }
